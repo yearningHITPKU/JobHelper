a12ecae7f909ca900c660b98729f91a845aefa8d
xww <hit_xiangwang@163.com>
2018-05-13 20:32:35 +0800

    change font size

--- application/common/Base.php
+++ application/common/Base.php
@@ -9,8 +9,6 @@
 namespace app\common;
 
 use think\Controller;
-use think\Db;
-use think\Debug;
 use think\Log;
 
 class Base extends Controller
@@ -18,62 +16,6 @@ class Base extends Controller
     public function _initialize()
     {
         parent::_initialize();
-        /*$accessToken = $this->get_access_token();
-        Debug::dump('asdsadsadsadasd: '.$accessToken);
-
-        if(!session('user_id')) {
-            //获得加密的rawdata
-            $getdata = input('get.');
-            if(!$getdata || !isset($getdata['rawdata'])) {
-                $this->error("请通过微信校园卡登录");
-            } else {
-                $rawdata = $getdata['rawdata'];
-                //设置post数据
-                $data = [];
-                $data['appid'] = Db::name('webset')->where('name', 'appid')->column('value')[0];
-                $data['appsecret'] = Db::name('webset')->where('name', 'appsecret')->column('value')[0];
-                $data['content'] = $rawdata;
-
-                //post请求获得用户信息
-                $curl = curl_init();
-                curl_setopt($curl, CURLOPT_URL, "https://icampus.ss.pku.edu.cn/iaaa/index.php/Home/OpenApi/decode");
-                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
-                if (! empty($data)) {
-                    curl_setopt($curl, CURLOPT_POST, 1);
-                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                }
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-                $output = curl_exec($curl);
-                $userdata = json_decode($output, true);
-                if($userdata['status'] == 1) {
-                    //设置session
-                    $sessionList = [
-                        'gender', 'type_name', 'degree_type_name', 'researcharea',
-                        'major_name', 'mail', 'telephone', 'birthday', 'politicsstatus', 'nation',
-                        'location', 'teacher_id', 'teacher_name', 'vice_teacher_id', 'vice_teacher_name',
-                        'startyearmonth', 'grade', 'hktwmacn', 'domain', 'imgurl'
-                    ];
-                    session('user_id', isset($userdata['data']['card_number']) ? $userdata['data']['card_number'] : $userdata['data']['studentid']);
-                    session('user_name', $userdata['data']['name']);
-                    foreach ($sessionList as $item) {
-                        session('user_'.$item, isset($userdata['data'][$item]) ? $userdata['data'][$item] : "");
-                    }
-                    //暂时不知道怎么分辨是校友还是在校生,都先设置为在校生
-                    $this->setUserType();
-                    //用户信息写入数据库
-                    if(session('user_type') == 0) {
-                        (new \app\frontend\model\Login())->insertStudent($userdata['data']['card_number'], $userdata['data']['name']);
-                    } else {
-                        (new \app\frontend\model\Login())->insertGraduate($userdata['data']['card_number'], $userdata['data']['name']);
-                    }
-
-                    $this->redirect('front/index/index');
-                } else {
-                    $this->error('请求连接失败');
-                }
-            }
-        }*/
     }
 
     public function get_user_data()
@@ -85,7 +27,6 @@ class Base extends Controller
             if(!$getdata || !isset($getdata['rawdata'])) {
                 $this->error("请通过微信校园卡登录");
             } else {
-                //$rawdata = '0e9652bfe142fb25e9893a4b1e4fc7b0f60af75122f3eb5b5e49a01acaa2d9228f0366c44dde29da80fae5537d45da4750400034e41af7cbb2fcc2ffb09ee744';
                 $rawdata = $getdata['rawdata'];
                 //设置post数据
                 $data = [];
