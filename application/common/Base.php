048a0124abd9777a80857bd2ec56205d7d6b19d1
xww <hit_xiangwang@163.com>
2018-05-13 20:05:29 +0800

    fix bug

--- application/common/Base.php
+++ application/common/Base.php
@@ -18,8 +18,7 @@ class Base extends Controller
     public function _initialize()
     {
         parent::_initialize();
-/*
-        $accessToken = $this->get_access_token();
+        /*$accessToken = $this->get_access_token();
         Debug::dump('asdsadsadsadasd: '.$accessToken);
 
         if(!session('user_id')) {
@@ -74,40 +73,6 @@ class Base extends Controller
                     $this->error('请求连接失败');
                 }
             }
-
-            $rawdata = '0e9652bfe142fb25e9893a4b1e4fc7b0f60af75122f3eb5b5e49a01acaa2d9228f0366c44dde29da80fae5537d45da4750400034e41af7cbb2fcc2ffb09ee744';
-            //设置post数据
-            $data = [];
-            $data['appid'] = Db::name('webset')->where('name', 'appid')->column('value')[0];
-            $data['appsecret'] = Db::name('webset')->where('name', 'appsecret')->column('value')[0];
-            $data['content'] = $rawdata;
-            $output = HttpService::http("https://icampus.ss.pku.edu.cn/iaaa/index.php/Home/OpenApi/decode", [], $data, "POST");
-            $userdata = json_decode($output, true);
-            Debug::dump($accessToken);
-            if($userdata['status'] == 1) {
-                //设置session
-                $sessionList = [
-                    'gender', 'type_name', 'degree_type_name', 'researcharea',
-                    'major_name', 'mail', 'telephone', 'birthday', 'politicsstatus', 'nation',
-                    'location', 'teacher_id', 'teacher_name', 'vice_teacher_id', 'vice_teacher_name',
-                    'startyearmonth', 'grade', 'hktwmacn', 'domain', 'imgurl'
-                ];
-                session('user_id', isset($userdata['data']['card_number']) ? $userdata['data']['card_number'] : $userdata['data']['studentid']);
-                session('user_name', $userdata['data']['name']);
-                foreach ($sessionList as $item) {
-                    session('user_'.$item, isset($userdata['data'][$item]) ? $userdata['data'][$item] : "");
-                }
-                //用户信息写入数据库
-                (new \app\frontend\model\User())->insertStudent($userdata['data']['card_number'], $userdata['data']['name']);
-
-                Debug::dump("444444444444444444444444444".$accessToken);
-                $this->redirect('frontend/index/index',['access_token' => $accessToken]);
-            } else {
-                $this->error('请求连接失败');
-            }
-        } else{
-            return 'app\common\Base: '.session('user_id')."  ".session('user_name')." ".$accessToken;
-            //$this->redirect('frontend/index/index',['access_token' => $accessToken]);
         }*/
     }
 
@@ -116,32 +81,37 @@ class Base extends Controller
         Log::record(session('user_id'));
         if(!session('user_id')) {
             //获得加密的rawdata
-
-            $rawdata = '0e9652bfe142fb25e9893a4b1e4fc7b0f60af75122f3eb5b5e49a01acaa2d9228f0366c44dde29da80fae5537d45da4750400034e41af7cbb2fcc2ffb09ee744';
-            //设置post数据
-            $data = [];
-            $data['appid'] = Db::name('webset')->where('name', 'appid')->column('value')[0];
-            $data['appsecret'] = Db::name('webset')->where('name', 'appsecret')->column('value')[0];
-            $data['content'] = $rawdata;
-            $output = HttpService::http("https://icampus.ss.pku.edu.cn/iaaa/index.php/Home/OpenApi/decode", [], $data, "POST");
-            $userdata = json_decode($output, true);
-            if($userdata['status'] == 1) {
-                //设置session
-                $sessionList = [
-                    'gender', 'type_name', 'degree_type_name', 'researcharea',
-                    'major_name', 'mail', 'telephone', 'birthday', 'politicsstatus', 'nation',
-                    'location', 'teacher_id', 'teacher_name', 'vice_teacher_id', 'vice_teacher_name',
-                    'startyearmonth', 'grade', 'hktwmacn', 'domain', 'imgurl'
-                ];
-                session('user_id', isset($userdata['data']['card_number']) ? $userdata['data']['card_number'] : $userdata['data']['studentid']);
-                session('user_name', $userdata['data']['name']);
-                foreach ($sessionList as $item) {
-                    session('user_'.$item, isset($userdata['data'][$item]) ? $userdata['data'][$item] : "");
-                }
-                //用户信息写入数据库
-                (new \app\frontend\model\User())->insertStudent($userdata['data']['card_number'], $userdata['data']['name']);
+            $getdata = input('get.');
+            if(!$getdata || !isset($getdata['rawdata'])) {
+                $this->error("请通过微信校园卡登录");
             } else {
-                $this->error('请求连接失败');
+                //$rawdata = '0e9652bfe142fb25e9893a4b1e4fc7b0f60af75122f3eb5b5e49a01acaa2d9228f0366c44dde29da80fae5537d45da4750400034e41af7cbb2fcc2ffb09ee744';
+                $rawdata = $getdata['rawdata'];
+                //设置post数据
+                $data = [];
+                $data['appid'] = Db::name('webset')->where('name', 'appid')->column('value')[0];
+                $data['appsecret'] = Db::name('webset')->where('name', 'appsecret')->column('value')[0];
+                $data['content'] = $rawdata;
+                $output = HttpService::http("https://icampus.ss.pku.edu.cn/iaaa/index.php/Home/OpenApi/decode", [], $data, "POST");
+                $userdata = json_decode($output, true);
+                if ($userdata['status'] == 1) {
+                    //设置session
+                    $sessionList = [
+                        'gender', 'type_name', 'degree_type_name', 'researcharea',
+                        'major_name', 'mail', 'telephone', 'birthday', 'politicsstatus', 'nation',
+                        'location', 'teacher_id', 'teacher_name', 'vice_teacher_id', 'vice_teacher_name',
+                        'startyearmonth', 'grade', 'hktwmacn', 'domain', 'imgurl'
+                    ];
+                    session('user_id', isset($userdata['data']['card_number']) ? $userdata['data']['card_number'] : $userdata['data']['studentid']);
+                    session('user_name', $userdata['data']['name']);
+                    foreach ($sessionList as $item) {
+                        session('user_' . $item, isset($userdata['data'][$item]) ? $userdata['data'][$item] : "");
+                    }
+                    //用户信息写入数据库
+                    (new \app\frontend\model\User())->insertStudent($userdata['data']['card_number'], $userdata['data']['name']);
+                } else {
+                    $this->error('请求连接失败');
+                }
             }
         }
     }
